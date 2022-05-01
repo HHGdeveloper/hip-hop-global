d17de5cfea480fb7a4eed9fec49a1f5ecdce728e
Sari Morninghawk <sari.morninghawk@gmail.com>
2022-05-01 18:54:40 -0400

    Thumbnails, sorting

--- src/pages/Videos/Content.js
+++ src/pages/Videos/Content.js
@@ -1,33 +1,118 @@
 import { useState, useEffect, useCallback, useRef } from "react";
 import CurrentVideo from './CurrentVideo.js';
-import { videoData } from './videoData.js';
+import { defaultData, oldestToNewestData, titleData, shortToLongData, longToShortData, sortButtons } from './videoData.js';
 import './Videos.scss';
 
+const msg = ['', 'No videos match your search criteria'];
+
 const Content = () => {
+    const [currentData, setCurrentData] = useState(defaultData);
     const [playerWidth, setPlayerWidth] = useState(null);
     const [playerHeight, setPlayerHeight] = useState(null);
-    const [playerURL, setPlayerURL] = useState(videoData[0].url);
+    const [playerURL, setPlayerURL] = useState(currentData[0].url);
     const [currentVideo, setCurrentVideo] = useState(0);
+    const [keywordsValue, setKeywordsValue] = useState('');
+    const [active, setActive] = useState(0);
+    const [message, setMessage] = useState(msg[0]);
     const playerRef = useRef();
 
     const handleThumbClick = index => {
-        setPlayerURL(videoData[index].url);
+        setPlayerURL(currentData[index].url);
         setCurrentVideo(index);
+        setMessage(msg[0]);
     };
+    const handleSort = (data, isActive, e) => {
+        if (e.target.classList.contains('active')) {
+            return null;
+        }
+
+        setKeywordsValue('')
+        setMessage(msg[0]);
+        setActive(isActive);
+        setCurrentData(data);
+        data.map((video, index) => {
+            if (video.url === playerURL) {
+                setCurrentVideo(index);
+            }
+        });
+    };
+    const handleKeywords = e => setKeywordsValue(e.target.value);
+
+    const filterVideos = () => {
+        const keywordFilter = 'keywords';
+        const titleFilter = 'title';
+        const keyword = keywordsValue.toLowerCase();
+        const keywordData = [];
+        const titleData = [];
+
+        setMessage(msg[0]);
+
+        currentData.map((data, index) => (
+            data.keywords.map(key => {
+                if (key === keyword) {
+                    keywordData.push(currentData[index]);
+                }
+            })
+        ));
+
+        currentData.map((data, index) => (
+            data.title.toLowerCase().split(' ').map(key => {
+                if (key === keyword) {
+                    titleData.push(currentData[index]);
+                }
+            })
+        ));
+
+        if (!keywordData.length && !titleData.length) {
+            setMessage(msg[1]);
+        }
+
+        if (!keywordData.length && titleData.length) {
+            setCurrentData(titleData);
+        }
+
+        if (!titleData.length && keywordData.length) {
+            setCurrentData(keywordData);
+        }
+
+        if (keywordData.length && titleData.length) {
+            const newData = keywordData.concat(titleData);
+
+            for (let i = 0; i < newData.length; ++i) {
+                for (let j = (i + 1); j < newData.length; ++j) {
+                    if(newData[i] === newData[j]) {
+                        newData.splice(j--, 1);
+                    }
+                
+                }
+            }
+
+            setCurrentData(newData);
+        }
+    }
 
     const renderThumbnails =
-        videoData.map((video, index) => {
+        currentData.map((video, index) => {
+            const displayLength = video.length.toString().replace('.', ':');
+
             if (index !== currentVideo) {
                 return (
                     <button className="thumbnail" key={index} onClick={() => handleThumbClick(index)}>
-                        <img alt="" src={require(`./assets/${video.thumbnail}.png`)} />
-                        <div className="time">{video.length}</div>
+                        <img alt="" src={require(`./thumbnails/${video.thumbnail}.png`)} />
+                        <div className="time">{displayLength}</div>
                         <div className="title">{video.title}</div>
                     </button>
                 );
             }
 
             return null;
+        });
+
+    const renderSortButtons =
+        sortButtons.map((button, index) => {
+            const cls = index === active ? 'btnSort active' : 'btnSort';
+
+            return <button className={cls} key={index} onClick={(e) => handleSort(button.data, index, e)}>{button.text}</button>
         });
 
     const getPlayerWidth = useCallback(() => {
@@ -60,18 +145,29 @@ const Content = () => {
     }, [getPlayerHeight]);
 
     return (
-        <div className="videoLayout">
-            <div className="playerWrapper">
-                <div className="player" ref={playerRef}>
-                    <CurrentVideo controls height={playerHeight} url={playerURL} width={playerWidth} />
+        <>
+            <div className="sortButtons">
+                {renderSortButtons}
+                <div className="keywordsWrapper">
+                    <div className="message">{message}</div>
+                    <label htmlFor="keywords" id="label-keywords">Keywords</label>
+                    <input id="keywords" type="text" value={keywordsValue} onChange={handleKeywords} />
+                    <button className="btnSort active" onClick={filterVideos}>Find videos</button>
                 </div>
-                <h2>{videoData[currentVideo].title}</h2>
-                <div className="time">{videoData[currentVideo].length}</div>
             </div>
-            <div className="thumbnails">
-                {renderThumbnails}
+            <div className="videoLayout">
+                <div className="playerWrapper">
+                    <div className="player" ref={playerRef}>
+                        <CurrentVideo controls height={playerHeight} url={playerURL} width={playerWidth} />
+                    </div>
+                    <h2>{currentData[currentVideo].title}</h2>
+                    <div className="time">{currentData[currentVideo].length.toString().replace('.', ':')}</div>
+                </div>
+                <div className="thumbnails">
+                    {renderThumbnails}
+                </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/pages/Videos/videoData.js
+++ src/pages/Videos/videoData.js
@@ -1,36 +1,38 @@
+import _ from 'lodash';
+
 const videoData = [
     {
         url: 'https://player.vimeo.com/video/684347399',
         thumbnail: 'RBEGlobalProducersDen',
         title: 'RBE Global Producers Den',
-        date: '',
-        length: '0:42',
+        date: '2022.04.01',
+        length: 0.42,
         description: '',
-        kewords: ['', '', '']
+        keywords: ['urban', 'movie', 'productions']
     }, {
         url: 'https://player.vimeo.com/video/688016509',
         thumbnail: 'HipHopAsia',
         title: 'Hip Hop Asia',
-        date: '',
-        length: '8:39',
+        date: '2022.03.01',
+        length: 8.39,
         description: '',
-        kewords: ['Asia', '', '']
+        keywords: ['Asia', 'music', 'other']
     }, {
         url: 'https://player.vimeo.com/video/687289852',
         thumbnail: 'BREADTVPromo',
         title: 'B.R.E.A.D TV Promo',
-        date: '',
-        length: '35:02',
+        date: '2022.02.01',
+        length: 35.02,
         description: '',
-        kewords: ['TV', '', '']
+        keywords: ['TV', 'show', 'breadxpress']
     }, {
         url: 'https://player.vimeo.com/video/687959396',
         thumbnail: 'Gee-BeeProductionsDemoReel',
         title: 'Gee-Bee Productions Demo Reel',
-        date: '',
-        length: '4:14',
+        date: '2022.01.01',
+        length: 4.14,
         description: '',
-        kewords: ['Film', 'Music videos', 'Video production', 'Demo reel']
+        keywords: ['film', 'music videos', 'video production', 'demo reel', 'movie', 'productions']
     }
     // , {
     //     url: 'https://player.vimeo.com/video/681017280',
@@ -39,8 +41,35 @@ const videoData = [
     //     date: '',
     //     length: '',
     //     description: '',
-    //     kewords: ['', '', '']
+    //     keywords: ['', '', '']
     // }
 ];
 
-export { videoData };
+const defaultData = _.orderBy(videoData, ['date'], ['desc']);
+const oldestToNewestData = _.orderBy(videoData, ['date'], ['asc']);
+const titleData = _.orderBy(videoData, ['title'], ['asc']);
+const shortToLongData = _.orderBy(videoData, ['length'], ['asc']);
+const longToShortData = _.orderBy(videoData, ['length'], ['desc']);
+
+// console.log(longToShortData);
+
+const sortButtons = [
+    {
+        text: 'Most recent',
+        data: defaultData
+    }, {
+        text: 'Oldest to newest',
+        data: oldestToNewestData
+    }, {
+        text: 'Alphabetical by title',
+        data: titleData
+    }, {
+        text: 'Shortest to longest',
+        data: shortToLongData
+    }, {
+        text: 'Longest to shortest',
+        data: longToShortData
+    }
+];
+
+export { defaultData, oldestToNewestData, titleData, shortToLongData, longToShortData, sortButtons };
